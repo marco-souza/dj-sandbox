d34aa17c3dec740f9fc6dbad47c85d8cdcfadf7e
Marco Antônio <ma.souza.junior@gmail.com>
2025-04-09 08:53:33 -0300

    enh: improve timerange creation

--- internal/shared/timerange.go
+++ internal/shared/timerange.go
@@ -9,8 +9,21 @@ type TimeRange struct {
 	End   string
 }
 
-// timerange to string to be used in the following command
-// yt-dlp -f bestvideo+bestaudio --download-sections "*00:32:53.2-00:32:55.5" --force-keyframes-at-cuts https://www.youtube.com/watch?v=S9HdPi9Ikhk --output "%UserProfile%\Downloads\%(extractor)s/%(title)s [%(resolution)s] [%(id)s] [f%(format_id)s].%(ext)s" --ffmpeg-location %UserProfile%\Downloads\ffmpeg-5.1.1-full_build\bin\ffmpeg.exe
+func NewTimeRange(start, end string) *TimeRange {
+	if start == "" {
+		start = "00:00:00.0"
+	}
+
+	if end == "" {
+		end = "inf"
+	}
+
+	return &TimeRange{
+		Start: start,
+		End:   end,
+	}
+}
+
 func (tr *TimeRange) String() string {
 	return fmt.Sprintf("*%s-%s", tr.Start, tr.End)
 }
--- internal/shared/timerange_test.go
+++ internal/shared/timerange_test.go
@@ -28,6 +28,31 @@ func TestTimeRangeString(t *testing.T) {
 			tr:       &s.TimeRange{Start: "00:00:00.0", End: "00:01:00.0"},
 			expected: "*00:00:00.0-00:01:00.0",
 		},
+		{
+			name:     "Empty start and end",
+			tr:       s.NewTimeRange("", ""),
+			expected: "*00:00:00.0-inf",
+		},
+		{
+			name:     "Empty start",
+			tr:       s.NewTimeRange("", "00:10:00.0"),
+			expected: "*00:00:00.0-00:10:00.0",
+		},
+		{
+			name:     "Empty end",
+			tr:       s.NewTimeRange("00:05:00.0", ""),
+			expected: "*00:05:00.0-inf",
+		},
+		{
+			name:     "Without miliseconds",
+			tr:       &s.TimeRange{Start: "01:00", End: "02:00"},
+			expected: "*01:00-02:00",
+		},
+		{
+			name:     "Without hours",
+			tr:       &s.TimeRange{Start: "00:30.0", End: "00:45.0"},
+			expected: "*00:30.0-00:45.0",
+		},
 	}
 
 	for _, tt := range tests {
